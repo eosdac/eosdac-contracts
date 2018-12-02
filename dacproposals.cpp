56453b7bf3100a29c5f7ab08256a089d3fff0a41
Dallas Johnson <dallasj001@gmail.com>
2018-12-02 01:11:51 +0000

    Adds a lot of the logic for the worker proposal voting, counting of votes up until the payment part.

--- dacproposals.cpp
+++ dacproposals.cpp
@@ -7,24 +7,115 @@
 using namespace eosio;
 using namespace std;
 
-    ACTION dacproposals::createprop(name proposer, string title, string summary, string desc, name arbitrator, asset payamount, string contenthash){
+    ACTION dacproposals::createprop(name proposer, string title, string summary, string desc, name arbitrator, extended_asset pay_amount, string content_hash){
+        require_auth(proposer);
 
+        proposals.emplace(proposer, [&](proposal &p) {
+            p.key = proposals.available_primary_key();
+            p.proposer = proposer;
+            p.arbitrator = arbitrator;
+            p.content_hash = content_hash;
+            p.pay_amount = pay_amount;
+            p.state = pending_approval;
+        });
     }
 
-    ACTION dacproposals::voteprop(name custodian, uint32_t proposal, uint8_t vote){
+    ACTION dacproposals::voteprop(name custodian, uint64_t proposal_id, VoteType vote){
+        require_auth(custodian);
+        
+        const proposal& prop = proposals.get(proposal_id, "Proposal not found.");
+        switch (prop.state) {
+            case pending_approval:
+                eosio_assert(vote == proposal_approve || vote == proposal_deny, "Invalid vote for the current proposal state.");
+                break;
+            case work_in_progress:
+                eosio_assert(vote == claim_approve || vote == claim_deny, "Invalid vote for the current proposal state.");
+                break;
+            default:
+                eosio_assert(false, "Invalid proposal state to accept votes.");
+        }
 
+        auto by_prop_and_voter = prop_votes.get_index<"propandvoter"_n>();
+        uint128_t joint_id = dacproposals::combine_ids(proposal_id, custodian.value);
+        auto vote_idx = by_prop_and_voter.find(joint_id);
+        if (vote_idx == by_prop_and_voter.end()) {
+            prop_votes.emplace(custodian, [&](proposalvote &v) {
+                v.vote_id = prop_votes.available_primary_key();
+                v.proposal_id = proposal_id;
+                v.voter = custodian;
+                v.vote = vote;
+            });
+        } else {
+            by_prop_and_voter.modify(vote_idx, custodian, [&](proposalvote &v) {
+                v.vote = vote;
+            });
+        }
     }
 
-    ACTION dacproposals::claim(name worker, uint64_t proposalid){
-
+    ACTION dacproposals::startwork(name proposer, uint64_t proposal_id) {
+        require_auth(proposer);
+        const proposal& prop = proposals.get(proposal_id, "Proposal not found.");
+        eosio_assert(prop.state == pending_approval,"Proposal is not in the pending approval state therefore cannot start work.");
+        auto by_voters = prop_votes.get_index<"proposal"_n>();
+        auto vote_idx = by_voters.find(proposal_id);
+        int16_t approved_count = 0;
+        int16_t deny_count = 0;
+        while(vote_idx != by_voters.end()) {
+            if (vote_idx->vote == proposal_approve) {
+                approved_count++;
+            }
+            if (vote_idx->vote == proposal_deny) {
+                deny_count++;
+            }        
+        }
+        eosio_assert(approved_count + deny_count >= configs.get().proposal_threshold, "Insufficient votes on worker proposal");
+        double percent_approval = double(approved_count) / double(deny_count) * 100.0;
+        eosio_assert(percent_approval >= configs.get().proposal_approval_threshold_percent, "Vote approval threshold not met.");
+        proposals.modify(prop, proposer, [&](proposal &p){
+            p.state = work_in_progress;
+        });
+        //TODO: Transfer funds to escrow account
+        print("Transfer funds to escrow account");
     }
 
-    ACTION dacproposals::cancel(name
-worker,
-uint64_t proposalid
-){
+    ACTION dacproposals::claim(name proposer, uint64_t proposal_id){
+        require_auth(proposer);
+        const proposal& prop = proposals.get(proposal_id, "Proposal not found.");
+        eosio_assert(prop.state == work_in_progress,"Proposal is not in the work-in-progress state therefore cannot be claimed as complete.");
+        auto by_voters = prop_votes.get_index<"proposal"_n>();
+        auto vote_idx = by_voters.find(proposal_id);
+        int16_t approved_count = 0;
+        int16_t deny_count = 0;
+        while(vote_idx != by_voters.end()) {
+            if (vote_idx->vote == claim_approve) {
+                approved_count++;
+            }
+            if (vote_idx->vote == claim_deny) {
+                deny_count++;
+            }        
+        }
+        eosio_assert(approved_count + deny_count >= configs.get().claim_threshold, "Insufficient votes on worker proposal to approve or deny claim.");
+        double percent_approval = double(approved_count) / double(deny_count) * 100.0;
+        eosio_assert(percent_approval >= configs.get().claim_approval_threshold_percent, "Vote claim approval threshold not met.");
+                proposals.modify(prop, proposer, [&](proposal &p){
+            p.state = work_in_progress;
+        });
+        //TODO: Transfer funds from escrow account
+        print("Transfer funds from escrow account to proposer.");
+    }
 
-}
+    ACTION dacproposals::cancel(name proposer, uint64_t proposal_id){
+        require_auth(proposer);
+        
+        auto &proposal = proposals.get(proposal_id, "Proposal not found.");
+        proposals.erase(proposal);
+
+        auto by_voters = prop_votes.get_index<"voter"_n>();
+        auto vote_idx = by_voters.find(proposal_id);
+        while (vote_idx != by_voters.end()) {
+            by_voters.erase(vote_idx);
+        }
+    }
 
 EOSIO_DISPATCH(dacproposals,
                 (createprop)
--- dacproposals.hpp
+++ dacproposals.hpp
@@ -1,4 +1,5 @@
 #include <eosiolib/multi_index.hpp>
+#include <eosiolib/singleton.hpp>
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp> 
 
@@ -6,51 +7,97 @@ using namespace eosio;
 using namespace std;
 
 CONTRACT dacproposals : public contract {
+    enum VoteType {  
+        none = 0,
+        // a vote type to indicate a custodian's approval of a worker proposal.
+        proposal_approve, 
+        // a vote type to indicate a custodian's denial of a worker proposal.
+        proposal_deny, 
+        // a vote type to indicate a custodian's acceptance of a worker proposal as completed.
+        claim_approve,
+        // a vote type to indicate a custodian's rejection of a worker proposal as completed.
+        claim_deny
+    };
+    enum ProposalState {  
+        pending_approval = 0, 
+        work_in_progress,
+        pending_claim, 
+        claim_approved, 
+        claim_denied
+    };
 
 public:
 
     dacproposals( name receiver, name code, datastream<const char*> ds )
-         : contract(receiver, code, ds), proposals(receiver, receiver.value) {}
+         : contract(receiver, code, ds), 
+         proposals(receiver, receiver.value),
+         prop_votes(receiver, receiver.value),
+         configs(receiver, receiver.value) {}
 
-    ACTION createprop(name proposer, string title, string summary, string desc, name arbitrator, asset payamount, string contenthash);
-    ACTION voteprop(name custodian, uint32_t proposal, uint8_t vote);
-    ACTION claim(name worker, uint64_t proposalid);
-    ACTION cancel(name worker, uint64_t proposalid);
+    ACTION createprop(name proposer, string title, string summary, string desc, name arbitrator, extended_asset pay_amount, string content_hash);
+    ACTION voteprop(name custodian, uint64_t proposal_id, VoteType vote);
+    ACTION startwork(name proposer, uint64_t proposal_id);
+    ACTION claim(name proposer, uint64_t proposal_id);
+    ACTION cancel(name proposer, uint64_t proposal_id);
 
 private:
 
+    TABLE config_type {
+        name service_account;
+        uint16_t proposal_threshold;
+        uint16_t proposal_approval_threshold_percent;
+        uint16_t claim_threshold;
+        uint16_t claim_approval_threshold_percent;
+    };
+
+    typedef eosio::singleton<"config"_n, config_type> configs_table;
+
+    configs_table configs;
+
     TABLE proposal {
-    uint32_t propid;
-    name proposer;
-    name arbitrator;
-    string contenthash; 
-    asset payamount;
+        uint64_t key;
+        name proposer;
+        name arbitrator;
+        string content_hash; 
+        extended_asset pay_amount;
+        ProposalState state;
 
-        uint64_t primary_key() const { return propid; }
+        uint64_t primary_key() const { return key; }
         uint64_t proposer_key() const { return proposer.value; }
-
+        uint64_t arbitrator_key() const { return arbitrator.value; }
 };
 
-typedef eosio::multi_index<"proposals"_n, indexed_by<"proposer"_n, eosio::const_mem_fun<proposal, uint64_t, &proposal::proposer_key>>> proposal_table;
+typedef eosio::multi_index<"proposals"_n, proposal,
+eosio::indexed_by<"proposer"_n, eosio::const_mem_fun<proposal, uint64_t, &proposal::proposer_key>>,
+eosio::indexed_by<"arbitrator"_n, eosio::const_mem_fun<proposal, uint64_t, &proposal::arbitrator_key>>
+> proposal_table;
 
 proposal_table proposals;
 
-
 TABLE proposalvote {
-    uint64_t propid;
+    uint64_t vote_id;
+    uint64_t proposal_id;
     name voter;
-    uint8_t vote; // enum
-    string commenthash;
+    VoteType vote;
+    string comment_hash;
 
-    uint64_t primary_key() const { return propid; }
+    uint64_t primary_key() const { return vote_id; }
+    uint64_t proposal_key() const { return proposal_id; }
+    uint64_t voter_key() const { return voter.value; }
+    uint128_t get_prop_and_voter() const { return combine_ids(proposal_id, voter.value); }
 };
 
-TABLE vote {
-    name voter;
-    name proxy;
-    asset stake;
+typedef eosio::multi_index<"propvotes"_n, proposalvote, 
+indexed_by<"proposal"_n, eosio::const_mem_fun<proposalvote, uint64_t, &proposalvote::voter_key>>,
+indexed_by<"voter"_n, eosio::const_mem_fun<proposalvote, uint64_t, &proposalvote::proposal_key>>,
+indexed_by<"propandvoter"_n, eosio::const_mem_fun<proposalvote, uint128_t, &proposalvote::get_prop_and_voter>>
+> proposal_vote_table;
 
-    name primary_key()const { return voter; }
-};
+proposal_vote_table prop_votes;
+
+// concatenation of ids example
+static const uint128_t combine_ids(const uint64_t &x, const uint64_t &y) {
+    return (uint128_t{x} << 64) | y;
+}
 
 };
