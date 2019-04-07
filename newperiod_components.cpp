58874ec6cf327356233cce673de0d2605814c353
Dallas Johnson <dallasj001@gmail.com>
2019-04-07 22:09:24 +0100

    Changed the permission changing action to use the `owner` permission to complete each of the inline actions.

--- newperiod_components.cpp
+++ newperiod_components.cpp
@@ -155,7 +155,7 @@ void daccustodian::setCustodianAuths() {
             .accounts = accounts
     };
 
-    action(permission_level{accountToChange, "active"_n},
+    action(permission_level{accountToChange, "owner"_n},
             "eosio"_n, "updateauth"_n,
            std::make_tuple(
                    accountToChange,
@@ -170,7 +170,7 @@ void daccustodian::setCustodianAuths() {
             .accounts = accounts
     };
 
-    action(permission_level{accountToChange, "active"_n},
+    action(permission_level{accountToChange, "owner"_n},
             "eosio"_n, "updateauth"_n,
            std::make_tuple(
                    accountToChange,
@@ -185,7 +185,7 @@ void daccustodian::setCustodianAuths() {
             .accounts = accounts
     };
 
-    action(permission_level{accountToChange, "active"_n},
+    action(permission_level{accountToChange, "owner"_n},
             "eosio"_n, "updateauth"_n,
            std::make_tuple(
                    accountToChange,
