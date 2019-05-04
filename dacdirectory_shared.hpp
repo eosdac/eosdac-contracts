a4b67d641146e7c9696ed8aa2a7ef843d10e86cb
Dallas Johnson <dallasj001@gmail.com>
2019-05-04 17:50:39 +0100

    Add convenience methods to dac struct to help access in other contracts.

--- dacdirectory_shared.hpp
+++ dacdirectory_shared.hpp
@@ -7,36 +7,54 @@
 using namespace eosio;
 using namespace std;
 
-enum account_type: uint8_t {
-    AUTH = 0,
-    TREASURY = 1,
-    CUSTODIAN = 2,
-    MSIGS = 3,
-    TOKEN = 4,
-    SERVICE = 5,
-    PROPOSALS = 6,
-    ESCROW = 7,
-    EXTERNAL = 254,
-    OTHER = 255
-};
+namespace dacdirectory {
 
-struct [[eosio::table("dacs"), eosio::contract("dacdirectory")]] dac {
-    name         owner;
-    name         dac_name;
-    string       title;
-    symbol       symbol;
-    map<uint8_t, string> refs;
-    map<uint8_t, eosio::name> accounts;
-    map<uint8_t, eosio::name> scopes;
+    enum account_type: uint8_t {
+        AUTH = 0,
+        TREASURY = 1,
+        CUSTODIAN = 2,
+        MSIGS = 3,
+        TOKEN = 4,
+        SERVICE = 5,
+        PROPOSALS = 6,
+        ESCROW = 7,
+        EXTERNAL = 254,
+        OTHER = 255
+    };
 
-    const eosio::name safe_scope_for_key(uint8_t key) {
-        return (scopes.find(key) != scopes.end()) ? scopes.at(key) : dac_name; 
+    struct AccountAndScope {
+        eosio::name account_name;
+        eosio::name dac_scope;
+    };
+
+    struct [[eosio::table("dacs"), eosio::contract("dacdirectory")]] dac {
+        name         owner;
+        name         dac_name;
+        string       title;
+        symbol       symbol;
+        map<uint8_t, string> refs;
+        map<uint8_t, eosio::name> accounts;
+        map<uint8_t, eosio::name> scopes;
+
+         eosio::name safe_scope_for_key(uint8_t key) const {
+            return (scopes.find(key) != scopes.end()) ? scopes.at(key) : dac_name; 
+        }
+
+         AccountAndScope account_and_scope( uint8_t type) const {
+            return AccountAndScope{accounts.at(type), safe_scope_for_key(type) };
+        }
+
+        uint64_t primary_key() const { return dac_name.value; }
+        uint64_t by_owner() const { return owner.value; }
+    };
+
+    typedef multi_index< "dacs"_n,  dac,
+                        indexed_by<"byowner"_n, const_mem_fun<dac, uint64_t, &dac::by_owner>>
+                        > dac_table;
+
+
+    const dac dac_for_id(name id) {
+        dac_table dactable = dac_table("dacdirectory"_n, "dacdirectory"_n.value);
+        return dactable.get(id.value, "dac with dac_name not found");
     }
-
-    uint64_t primary_key() const { return dac_name.value; }
-    uint64_t by_owner() const { return owner.value; }
-};
-
-typedef multi_index< "dacs"_n,  dac,
-                    indexed_by<"byowner"_n, const_mem_fun<dac, uint64_t, &dac::by_owner>>
-                    > dac_table;
+}
