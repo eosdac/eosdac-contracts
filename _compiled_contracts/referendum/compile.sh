ec79c43032ed8b0e67c6debb522d78946c9e723c
Michael Yeates <michaeljyeates@gmail.com>
2019-09-03 20:06:35 +0100

    Remove workaround for optional argument

--- _compiled_contracts/referendum/compile.sh
+++ _compiled_contracts/referendum/compile.sh
@@ -3,8 +3,3 @@
 source `dirname $BASH_SOURCE`/common.sh
 
 eosio-cpp -o `dirname $BASH_SOURCE`/$CONTRACT.wasm $CONTRACT.cpp
-
-# replace action? with action in the abi, all the tools seem to have trouble with it
-
-sed 's/action\?/action/g' `dirname $BASH_SOURCE`/$CONTRACT.abi > `dirname $BASH_SOURCE`/$CONTRACT.abi.tmp
-mv `dirname $BASH_SOURCE`/$CONTRACT.abi.tmp  `dirname $BASH_SOURCE`/$CONTRACT.abi
