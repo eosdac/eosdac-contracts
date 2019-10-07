65c2d4eb1dcc8f4cb2987d0583ccbda967b976cb
Dallas Johnson <dallasj001@gmail.com>
2019-10-07 01:17:24 +0100

    Adds tests for eosdactokens - still more needed for good coverage

---
+++ eosdactokens/eosdactokens.test.ts
@@ -0,0 +1,250 @@
+import * as l from 'lamington';
+
+import { SharedTestObjects, initAndGetSharedObjects, candidates, regmembers, Account_type } from '../TestHelpers';
+import * as chai from 'chai';
+chai.use(require('chai-datetime'));
+
+// import { equalDate } from 'chai-datetime';
+import { EosdactokensStakeConfig } from './eosdactokens';
+
+describe('EOSDacTokens', () => {
+  let shared: SharedTestObjects;
+  let issuer: l.Account;
+  let otherAccount: l.Account;
+
+  before(async () => {
+    shared = await initAndGetSharedObjects();
+    issuer = await l.AccountManager.createAccount();
+    otherAccount = await l.AccountManager.createAccount();
+  });
+
+  context('create token', async () => {
+    it('with invalid token symbol should fail', async () => {
+      await l.assertEOSMessageError(shared.dac_token_contract.create(issuer.name, '10000.0000 asdf', false, { from: issuer }), 'ERR::CREATE_INVALID_SYMBOL');
+    });
+    it('with negative token quantity should fail', async () => {
+      await l.assertEOSMessageError(shared.dac_token_contract.create(issuer.name, `${2 ** 62 + 10} ABC`, false, { from: issuer }), 'ERR::CREATE_INVALID_SUPPLY');
+    });
+    it('with negative token quantity should fail', async () => {
+      await l.assertEOSMessageError(shared.dac_token_contract.create(issuer.name, '-10000.0000 ABC', false, { from: issuer }), 'ERR::CREATE_MAX_SUPPLY_MUST_BE_POSITIVE');
+    });
+    it('with valid asset should succeed', async () => {
+      await shared.dac_token_contract.create(issuer.name, '100000.0000 ABC', false, { from: issuer });
+    });
+    it('with negative token quantity should fail', async () => {
+      await l.assertEOSMessageError(shared.dac_token_contract.create(issuer.name, '1000000.0000 ABC', false, { from: issuer }), 'ERR::CREATE_EXISITNG_SYMBOL');
+    });
+  });
+  context('issue token', async () => {
+    it('with invalid token symbol should fail', async () => {
+      await l.assertEOSMessageError(shared.dac_token_contract.issue(issuer.name, '10000.0000 sdasd', 'some memo', { from: issuer }), 'ERR::ISSUE_INVALID_SYMBOL');
+    });
+    it('with non existing token should fail', async () => {
+      await l.assertEOSMessageError(shared.dac_token_contract.issue(issuer.name, '10000.0000 CBA', 'some memo', { from: issuer }), 'ERR::ISSUE_NON_EXISTING_SYMBOL:');
+    });
+    it('with invalid quantity should fail', async () => {
+      await l.assertEOSMessageError(shared.dac_token_contract.issue(issuer.name, `${2 ** 62 + 10} ABC`, 'some memo', { from: issuer }), 'ERR::ISSUE_INVALID_QUANTITY:');
+    });
+    it('with negative quantity should fail', async () => {
+      await l.assertEOSMessageError(shared.dac_token_contract.issue(issuer.name, `-1000.0000 ABC`, 'some memo', { from: issuer }), 'ERR::ISSUE_NON_POSITIVE');
+    });
+    it('with invalid precision should fail', async () => {
+      await l.assertEOSMessageError(shared.dac_token_contract.issue(issuer.name, `1000.00 ABC`, 'some memo', { from: issuer }), 'ERR::ISSUE_INVALID_PRECISION:');
+    });
+    it('to issuer should increase the issuer balance', async () => {
+      shared.dac_token_contract.issue(issuer.name, `1000.0000 ABC`, 'some memo', { from: issuer });
+      await l.assertRowsEqual(shared.dac_token_contract.statTable({ scope: '4,ABC' }), []);
+
+      await l.assertRowsEqual(shared.dac_token_contract.accountsTable({ scope: issuer.name }), [{ balance: '1000.0000 ABC' }]);
+    });
+    context('to other', async () => {
+      context('without a dac', async () => {
+        it('should fail', async () => {
+          await l.assertEOSMessageError(shared.dac_token_contract.issue(otherAccount.name, `1200.0000 ABC`, 'some memo', { from: issuer }), 'ERR::DAC_NOT_FOUND_SYMBOL');
+        });
+      });
+      context('with a dac', async () => {
+        before(async () => {
+          await shared.dacdirectory_contract.regdac(
+            shared.auth_account.name,
+            'abcdac',
+            { contract: shared.dac_token_account.name, symbol: '4,ABC' },
+            'abc dac_title',
+            [],
+            [{ key: Account_type.AUTH, value: shared.auth_account.name }, { key: Account_type.CUSTODIAN, value: shared.daccustodian_account.name }],
+            {
+              auths: [{ actor: shared.auth_account.name, permission: 'active' }]
+            }
+          );
+        });
+        it('should increase the other balance', async () => {
+          await shared.dac_token_contract.issue(otherAccount.name, `1200.0000 ABC`, 'some memo', { from: issuer });
+          await l.assertRowsEqual(shared.dac_token_contract.statTable({ scope: '4,ABC' }), []);
+
+          await l.assertRowsEqual(shared.dac_token_contract.accountsTable({ scope: otherAccount.name }), [{ balance: '1200.0000 ABC' }]);
+        });
+      });
+    });
+  });
+  context('Staking', async () => {
+    let staker: l.Account;
+    before(async () => {
+      staker = await l.AccountManager.createAccount();
+      await shared.dac_token_contract.issue(staker.name, '1000.0000 ABC', 'initial issued tokens', { from: issuer });
+    });
+    context('stake', async () => {
+      context('stake without staking enabled', async () => {
+        it('should fail', async () => {
+          await l.assertEOSMessageError(shared.dac_token_contract.stake(staker.name, '100.0000 ABC', { from: staker }), 'ERR::STAKING_NOT_ENABLED');
+        });
+      });
+      context('with staking enabled', async () => {
+        before(async () => {
+          shared.dac_token_contract.stakeconfig({ enabled: true, min_stake_time: 13, max_stake_time: 20 }, '4,ABC', { from: shared.auth_account });
+        });
+        it('with invalid quantity should fail', async () => {
+          await l.assertEOSMessageError(shared.dac_token_contract.stake(staker.name, `${10 + 2 ** 62}` + ' ABC', { from: staker }), 'DAC_NOT_FOUND_SYMBOL'); // This is not testable because a DAC cannot be found with in invalid quantity first
+        });
+        it('with negative quantity should fail', async () => {
+          await l.assertEOSMessageError(shared.dac_token_contract.stake(staker.name, `-1000.0000 ABC`, { from: staker }), 'STAKE_NON_POSITIVE_QTY');
+        });
+        it('with more than available balance should fail', async () => {
+          await l.assertEOSMessageError(shared.dac_token_contract.stake(staker.name, `1001.0000 ABC`, { from: staker }), 'ERR::STAKE_MORE_LIQUID');
+        });
+        it('with correct amount should succeed', async () => {
+          await shared.dac_token_contract.stake(staker.name, '100.0000 ABC', { from: staker });
+          await l.assertRowsEqual(shared.dac_token_contract.stakesTable({ scope: 'abcdac' }), [{ account: staker.name, stake: '100.0000 ABC' }]);
+        });
+        it('staking again should add more', async () => {
+          await shared.dac_token_contract.stake(staker.name, '50.0000 ABC', { from: staker });
+          await l.assertRowsEqual(shared.dac_token_contract.stakesTable({ scope: 'abcdac' }), [{ account: staker.name, stake: '150.0000 ABC' }]);
+        });
+        it('with more than liquid balance should fail', async () => {
+          await l.assertEOSMessageError(shared.dac_token_contract.stake(staker.name, `851.0000 ABC`, { from: staker }), 'ERR::STAKE_MORE_LIQUID:');
+        });
+      });
+    });
+    context('unstake', async () => {
+      context('without staking enabled', async () => {
+        before(async () => {
+          await shared.dac_token_contract.stakeconfig({ enabled: false, min_stake_time: 13, max_stake_time: 20 }, '4,ABC', { from: shared.auth_account });
+        });
+        it('should fail', async () => {
+          await l.assertEOSMessageError(shared.dac_token_contract.unstake(staker.name, '75.0000 ABC', { from: staker }), 'STAKING_NOT_ENABLED');
+        });
+      });
+      context('with staking enabled', async () => {
+        before(async () => {
+          await shared.dac_token_contract.stakeconfig({ enabled: true, min_stake_time: 5, max_stake_time: 20 }, '4,ABC', { from: shared.auth_account });
+        });
+        it('should fail for negative quantity', async () => {
+          await l.assertEOSMessageError(shared.dac_token_contract.unstake(staker.name, '-75.0000 ABC', { from: staker }), 'UNSTAKE_NON_POSITIVE_QTY');
+        });
+        it('should fail for amount in excess of stake', async () => {
+          await l.assertEOSMessageError(shared.dac_token_contract.unstake(staker.name, '151.0000 ABC', { from: staker }), 'UNSTAKE_OVER');
+        });
+        it('should fail for no stake', async () => {
+          await l.assertEOSMessageError(shared.dac_token_contract.unstake(otherAccount.name, '75.0000 ABC', { from: otherAccount }), 'NO_STAKE_FOUND');
+        });
+        it('should succeed', async () => {
+          await shared.dac_token_contract.unstake(staker.name, '74.0000 ABC', { from: staker });
+          await shared.dac_token_contract.unstake(staker.name, '30.0000 ABC', { from: staker });
+          await l.assertRowsEqual(shared.dac_token_contract.stakesTable({ scope: 'abcdac' }), [{ account: staker.name, stake: '46.0000 ABC' }]);
+          let unstakeRow = await shared.dac_token_contract.unstakesTable({ scope: 'abcdac' });
+          let releaseDate = unstakeRow.rows[0].release_time;
+          console.log('unstake Tbale : ' + releaseDate.toUTCString());
+
+          chai.expect(unstakeRow.rows[0]).to.include({ account: staker.name, stake: '74.0000 ABC', key: 0 });
+          // JSON.stringify(releaseDate, null, 3) + ' raw: ' + releaseDate + ' pros: ' + releaseDate.toISOString());
+          let nowDate = new Date();
+          let futureDate = new Date();
+          nowDate.setTime(nowDate.getTime() - 20000);
+          futureDate.setTime(futureDate.getTime() + 20000);
+          chai.expect(releaseDate).afterTime(nowDate);
+          chai.expect(releaseDate).beforeTime(futureDate);
+          chai.expect(unstakeRow.rows.length).to.equal(2);
+        });
+      });
+    });
+  });
+
+  context('transfer', async () => {
+    let sender: l.Account;
+    let receiver: l.Account;
+    before(async () => {
+      sender = await l.AccountManager.createAccount();
+      receiver = await l.AccountManager.createAccount();
+      await shared.dac_token_contract.issue(sender.name, '1000.0000 ABC', 'initial issued tokens', { from: issuer });
+    });
+    context('with staking enabled', async () => {
+      context('with nothing staked from sender', async () => {
+        it('should succeed', async () => {
+          await shared.dac_token_contract.transfer(sender.name, receiver.name, '10.0000 ABC', 'memo', { from: sender });
+        });
+      });
+      context('with some staked but not enough liquid for transfer', async () => {
+        before(async () => {
+          await shared.dac_token_contract.stake(sender.name, '100.0000 ABC', { from: sender });
+        });
+        it('should fail', async () => {
+          l.assertEOSMessageError(shared.dac_token_contract.transfer(sender.name, receiver.name, '900.0000 ABC', 'memo', { from: sender }), 'ERR::BALANCE_STAKED');
+        });
+      });
+      context('with some staked but enough liquid for transfer', async () => {
+        it('should succeed', async () => {
+          await shared.dac_token_contract.transfer(sender.name, receiver.name, '100.0000 ABC', 'memo', { from: sender });
+        });
+      });
+      context('with some staked and exactly enough liquid for transfer', async () => {
+        it('should succeed', async () => {
+          await shared.dac_token_contract.transfer(sender.name, receiver.name, '790.0000 ABC', 'memo', { from: sender });
+        });
+      });
+    });
+  });
+});
+
+/*
+
+
+  it('Registering as a member', async function() {
+    let terms_hash: string = '1df37bdb72c0be963ef2bdfe9b7ef10b';
+    const terms_url: string =
+      'https://raw.githubusercontent.com/eosdac/eosdac-constitution/master/boilerplate_constitution.md';
+    const terms_incorrect = '19254f2be00396b63c713e5b06e7dd36';
+
+    await dao_token_contract.newmemtermse(terms_url, terms_hash, dac_id, {
+      from: sharedTestObjects.auth_account
+    });
+
+    await l.assertEOSError(
+      dao_token_contract.memberrege(donor1.name, terms_hash, dac_id, {
+        from: donor1
+      }),
+      'eosio_assert_message_exception',
+      'Attempt to register with no hash asserts'
+    );
+
+    await l.assertEOSError(
+      dao_token_contract.memberrege(donor1.name, terms_hash, dac_id, {
+        from: donor1
+      }),
+      'eosio_assert_message_exception',
+      'Attempt to register with no member terms configured asserts'
+    );
+
+    await l.assertEOSError(
+      dao_token_contract.memberrege(donor1.name, terms_incorrect, dac_id, {
+        from: donor1
+      }),
+      'eosio_assert_message_exception',
+      'Attempt to register with incorrect hash asserts'
+    );
+
+    // registration should succeed
+    await dao_token_contract.memberrege(donor1.name, terms_hash, dac_id, {
+      from: donor1
+    });
+  });
+});
+*/
---
+++ eosdactokens/eosdactokens.ts
@@ -0,0 +1,204 @@
+// =====================================================
+// WARNING: GENERATED FILE
+//
+// Any changes you make will be overwritten by Lamington
+// =====================================================
+
+import { Account, Contract, GetTableRowsOptions, ActorPermission, ExtendedAsset, ExtendedSymbol, TableRowsResult } from 'lamington';
+
+// Table row types
+export interface EosdactokensAccount {
+	balance: string;
+}
+
+export interface EosdactokensBurn {
+	from: string|number;
+	quantity: string;
+}
+
+export interface EosdactokensCancel {
+	unstake_id: number;
+	token_symbol: string;
+}
+
+export interface EosdactokensClearold {
+	batch_size: number;
+}
+
+export interface EosdactokensClose {
+	owner: string|number;
+	symbol: string;
+}
+
+export interface EosdactokensCreate {
+	issuer: string|number;
+	maximum_supply: string;
+	transfer_locked: boolean;
+}
+
+export interface EosdactokensCurrencyStats {
+	supply: string;
+	max_supply: string;
+	issuer: string|number;
+	transfer_locked: boolean;
+}
+
+export interface EosdactokensIssue {
+	to: string|number;
+	quantity: string;
+	memo: string;
+}
+
+export interface EosdactokensMember {
+	sender: string|number;
+	agreedtermsversion: number;
+}
+
+export interface EosdactokensMemberreg {
+	sender: string|number;
+	agreedterms: string;
+}
+
+export interface EosdactokensMemberrege {
+	sender: string|number;
+	agreedterms: string;
+	dac_id: string|number;
+}
+
+export interface EosdactokensMemberunreg {
+	sender: string|number;
+}
+
+export interface EosdactokensMemberunrege {
+	sender: string|number;
+	dac_id: string|number;
+}
+
+export interface EosdactokensMigrate {
+	batch: number;
+}
+
+export interface EosdactokensNewmemterms {
+	terms: string;
+	hash: string;
+}
+
+export interface EosdactokensNewmemtermse {
+	terms: string;
+	hash: string;
+	dac_id: string|number;
+}
+
+export interface EosdactokensRefund {
+	unstake_id: number;
+	token_symbol: string;
+}
+
+export interface EosdactokensStake {
+	account: string|number;
+	quantity: string;
+}
+
+export interface EosdactokensStakeConfig {
+	enabled: boolean;
+	min_stake_time: number;
+	max_stake_time: number;
+}
+
+export interface EosdactokensStakeInfo {
+	account: string|number;
+	stake: string;
+}
+
+export interface EosdactokensStakeconfig {
+	config: string;
+	token_symbol: string;
+}
+
+export interface EosdactokensStaketime {
+	account: string|number;
+	unstake_time: number;
+	token_symbol: string;
+}
+
+export interface EosdactokensStaketimeInfo {
+	account: string|number;
+	delay: number;
+}
+
+export interface EosdactokensTermsinfo {
+	terms: string;
+	hash: string;
+	version: number;
+}
+
+export interface EosdactokensTransfer {
+	from: string|number;
+	to: string|number;
+	quantity: string;
+	memo: string;
+}
+
+export interface EosdactokensUnlock {
+	unlock: string;
+}
+
+export interface EosdactokensUnstake {
+	account: string|number;
+	quantity: string;
+}
+
+export interface EosdactokensUnstakeInfo {
+	key: number;
+	account: string|number;
+	stake: string;
+	release_time: Date;
+}
+
+export interface EosdactokensUpdateterms {
+	termsid: number;
+	terms: string;
+}
+
+export interface EosdactokensUpdatetermse {
+	termsid: number;
+	terms: string;
+	dac_id: string|number;
+}
+
+export interface Eosdactokens extends Contract {
+	// Actions
+	burn(from: string|number, quantity: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	cancel(unstake_id: number, token_symbol: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	clearold(batch_size: number, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	close(owner: string|number, symbol: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	create(issuer: string|number, maximum_supply: string, transfer_locked: boolean, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	issue(to: string|number, quantity: string, memo: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	memberreg(sender: string|number, agreedterms: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	memberrege(sender: string|number, agreedterms: string, dac_id: string|number, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	memberunreg(sender: string|number, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	memberunrege(sender: string|number, dac_id: string|number, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	migrate(batch: number, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	newmemterms(terms: string, hash: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	newmemtermse(terms: string, hash: string, dac_id: string|number, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	refund(unstake_id: number, token_symbol: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	stake(account: string|number, quantity: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	stakeconfig(config: EosdactokensStakeConfig, token_symbol: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	staketime(account: string|number, unstake_time: number, token_symbol: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	transfer(from: string|number, to: string|number, quantity: string, memo: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	unlock(unlock: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	unstake(account: string|number, quantity: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	updateterms(termsid: number, terms: string, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	updatetermse(termsid: number, terms: string, dac_id: string|number, options?: { from?: Account, auths?: ActorPermission[] }): Promise<any>;
+	
+	// Tables
+	accountsTable(options?: GetTableRowsOptions): Promise<TableRowsResult<EosdactokensAccount>>;
+	membersTable(options?: GetTableRowsOptions): Promise<TableRowsResult<EosdactokensMember>>;
+	membertermsTable(options?: GetTableRowsOptions): Promise<TableRowsResult<EosdactokensTermsinfo>>;
+	stakeconfigTable(options?: GetTableRowsOptions): Promise<TableRowsResult<EosdactokensStakeConfig>>;
+	stakesTable(options?: GetTableRowsOptions): Promise<TableRowsResult<EosdactokensStakeInfo>>;
+	staketimeTable(options?: GetTableRowsOptions): Promise<TableRowsResult<EosdactokensStaketimeInfo>>;
+	statTable(options?: GetTableRowsOptions): Promise<TableRowsResult<EosdactokensCurrencyStats>>;
+	unstakesTable(options?: GetTableRowsOptions): Promise<TableRowsResult<EosdactokensUnstakeInfo>>;
+}
+
