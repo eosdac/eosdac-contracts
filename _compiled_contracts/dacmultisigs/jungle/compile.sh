6e45202624a9c86bc0ddfa547d871924d9956c0f
Michael Yeates <michaeljyeates@gmail.com>
2019-11-25 12:15:45 +0000

    Remove eosio multisig contract override

--- _compiled_contracts/dacmultisigs/jungle/compile.sh
+++ _compiled_contracts/dacmultisigs/jungle/compile.sh
@@ -2,4 +2,4 @@
 
 source `dirname $BASH_SOURCE`/../common.sh
 mkdir `dirname $BASH_SOURCE`/$CONTRACT
-eosio-cpp -DMSIGCONTRACT=eosiomsigold -o `dirname $BASH_SOURCE`/$CONTRACT/$CONTRACT.wasm $CONTRACT.cpp -I.
+eosio-cpp -o `dirname $BASH_SOURCE`/$CONTRACT/$CONTRACT.wasm $CONTRACT.cpp -I.
