a5988abed3f8f276a454facb78edc452a8d66584
Michael Yeates <michaeljyeates@gmail.com>
2020-01-24 10:57:54 +0000

    Optimise clearprop and add clearconfig action

--- dacproposals/dacproposals.cpp
+++ dacproposals/dacproposals.cpp
@@ -299,6 +299,16 @@ namespace eosdac {
         configs.set(new_config, auth_account);
     }
 
+    ACTION dacproposals::clearconfig(name dac_id) {
+        auto auth_account = dacdir::dac_for_id(dac_id).account_for_type(dacdir::AUTH);
+        require_auth(auth_account);
+
+        configs_table configs(_self, dac_id.value);
+        if (configs.exists()){
+            configs.remove();
+        }
+    }
+
     ACTION dacproposals::clearexpprop(name proposal_id, name dac_id) {
         proposal_table proposals(_self, dac_id.value);
 
@@ -376,19 +386,16 @@ namespace eosdac {
     void dacproposals::clearprop(const proposal& proposal, name dac_id){
 
         proposal_table proposals(_self, dac_id.value);
+        auto prop_to_erase = proposals.find(proposal.proposal_id.value);
+
+        check(prop_to_erase != proposals.end(), "ERR::PROPOSAL_NOT_FOUND::Proposal not found");
 
         // Remove all the votes associated with that proposal.
         proposal_vote_table prop_votes(_self, dac_id.value);
-        auto by_voters = prop_votes.get_index<"proposal"_n>();
-        auto itr = by_voters.find(proposal.proposal_id.value);
-        while(itr != by_voters.end() && itr->proposal_id) {
-            auto proposal_id = *(itr->proposal_id);
-            if (proposal_id == proposal.proposal_id){
-                print(itr->voter);
-                itr = by_voters.erase(itr);
-            }
+        auto by_proposal = prop_votes.get_index<"proposal"_n>();
+        for (auto itr = by_proposal.lower_bound(proposal.proposal_id.value), end_itr = by_proposal.upper_bound(proposal.proposal_id.value); itr != end_itr && itr != by_proposal.end(); ++itr) {
+            itr = by_proposal.erase(itr);
         }
-        auto prop_to_erase = proposals.find(proposal.proposal_id.value);
 
         proposals.erase(prop_to_erase);
     }
