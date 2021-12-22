7593bd64666cf1e6829549c9f5126f1a26989210
Angelo Laub <angelolaub@gmail.com>
2021-12-22 21:15:46 +0100

    add utility functions for more beautiful and more helpful error messages

--- contract-shared-headers/common_utilities.hpp
+++ contract-shared-headers/common_utilities.hpp
@@ -2,11 +2,14 @@
 #define COMMON_UTILITIES_H
 #include <eosio/eosio.hpp>
 #include <eosio/symbol.hpp>
+#include <eosio/asset.hpp>
+
+using namespace eosio;
+using namespace std;
 
 namespace eosdac {
 
-    using namespace eosio;
-    using namespace std;
+    
 
     // Utility to combine ids to help with indexing.
     uint128_t combine_ids(const uint8_t &boolvalue, const uint64_t &longValue) {
@@ -33,5 +36,46 @@ namespace eosdac {
     static const __uint128_t raw_from_extended_symbol(const extended_symbol &symbol) {
         return (uint128_t{symbol.get_contract().value} << 64) | symbol.get_symbol().code().raw();
     }
+    
+  
+
+    
 } // namespace eosdac
+
+/*
+ * Polymorphic helper to convert common EOS.IO types to string
+ */
+template<typename T>
+inline string toString(const T& x) {
+  if constexpr(is_same<T, string>::value) {
+    return x;
+  } else if constexpr(is_same<T, eosio::name>::value || is_same<T, eosio::asset>::value){
+    return x.to_string();
+  } else if constexpr(is_same<T, eosio::symbol>::value) {
+    return x.code().to_string();
+  } else {
+    return to_string(x);
+  }
+} 
+
+/*
+ * C++'s missing format string function :-)
+ */
+template<typename... Args>
+inline string fmt(const string& format, Args const& ... args){
+  char buf[512];
+  snprintf(buf, sizeof(buf), format.c_str(), toString(args).c_str()...);
+  return buf;
+}
+/* eosio::check overload that allows passing a format string for more 
+ * helpful error messages.
+ */
+template<typename... Args>
+inline void check(bool pred, const string& format, Args const& ... args) {
+  if(!pred) {
+    const auto msg = fmt(format, args...);
+    check(pred, msg.c_str());    
+  }
+}
+
 #endif
