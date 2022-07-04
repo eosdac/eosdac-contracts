34e64eb6afdad8d4389b8bd392edef732ca50fef
Angelo Laub <angelolaub@gmail.com>
2022-07-04 11:57:22 +0200

    use safemath for avg_vote_time calculation

--- contracts/daccustodian/privatehelpers.cpp
+++ contracts/daccustodian/privatehelpers.cpp
@@ -39,16 +39,13 @@ void daccustodian::updateVoteWeight(
 
 time_point_sec daccustodian::calculate_avg_vote_time_stamp(const time_point_sec vote_time_before,
     const time_point_sec vote_time_stamp, const int64_t weight, const uint64_t total_votes) {
-    check(total_votes != 0, "division by zero, total_votes is 0");
-    const auto delta_seconds =
-        abs(int128_t(vote_time_stamp.sec_since_epoch()) - int128_t(vote_time_before.sec_since_epoch())) *
-        int128_t(weight) / int128_t(total_votes);
-    const auto max_amount = std::numeric_limits<int64_t>::max();
-    check(delta_seconds <= max_amount, "multiplication overflow");
-    check(delta_seconds >= -max_amount, "multiplication underflow");
-    const auto new_seconds = int128_t(vote_time_before.sec_since_epoch()) + delta_seconds;
-    check(new_seconds <= std::numeric_limits<uint32_t>::max(), "new_seconds does not fit into a uint32_t");
-    return time_point_sec{uint32_t(new_seconds)};
+
+    const auto initial    = S{vote_time_before.sec_since_epoch()}.to<int128_t>();
+    const auto current    = S{vote_time_stamp.sec_since_epoch()}.to<int128_t>();
+    const auto time_delta = abs(current - initial);
+
+    const auto new_seconds = initial + time_delta * S{weight}.to<int128_t>() / S{total_votes}.to<int128_t>();
+    return time_point_sec{new_seconds.to<uint32_t>()};
 }
 
 void daccustodian::updateVoteWeights(const vector<name> &votes, const time_point_sec vote_time_stamp,
