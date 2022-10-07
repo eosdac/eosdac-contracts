020541126e31cdf7d07a322f208eee7b43aa2409
Angelo Laub <angelolaub@gmail.com>
2022-10-07 13:44:50 +0400

    simplify stakevote fomula

--- contracts/stakevote/stakevote.hpp
+++ contracts/stakevote/stakevote.hpp
@@ -10,16 +10,13 @@ using namespace eosio;
 using namespace eosdac;
 using namespace std;
 
-static constexpr auto time_divisor          = S{10}.ipow(8).to<double>();
-static constexpr auto stake_duration_factor = S{10}.to<double>();
-
 CONTRACT stakevote : public contract {
   public:
     using contract::contract;
 
     struct [[eosio::table("config"), eosio::contract("stakevote")]] config_item {
         // time multiplier is measured in 10^-8 1 == 0.00000001
-        int64_t            time_multiplier = time_divisor;
+        int64_t            time_multiplier;
         static config_item get_current_configs(eosio::name account, eosio::name scope) {
             check(config_container(account, scope.value).exists(), "Stake config not set.");
             return config_container(account, scope.value).get();
